74a76610f8e561ae2dbbb37ffa5f9a2cedcc6e97
basyura <basyura@gmail.com>
2019-04-06 20:33:58 +0900

    change namespace

--- EleveProjectTemplate/Views/SampleView.xaml.cs
+++ EleveProjectTemplate/Views/SampleView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace $safeprojectname$
+namespace $safeprojectname$.Views
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
