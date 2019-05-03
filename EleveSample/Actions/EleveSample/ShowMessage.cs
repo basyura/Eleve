4fce302abe17e5ebd9311e7f3f97ab6b0e51227e
basyura <basyura@gmail.com>
2019-05-03 15:15:13 +0900

    added MessageBox.Show

---
+++ EleveSample/Actions/EleveSample/ShowMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Eleve;
+
+namespace EleveSample.Actions.EleveSample
+{
+    public class ShowMessage : EleveSampleActionBase
+    {
+        public override Task<ActionResult> Execute(object sender, EventArgs args, object obj)
+        {
+            MessageBoxResult res = MessageBox.Show("hello", "select", MessageBoxButton.OKCancel);
+
+            if (res == MessageBoxResult.OK)
+            {
+                ViewModel.Message = "OK";
+            }
+            else
+            {
+                ViewModel.Message = "not OK";
+            }
+
+            return SuccessTask;
+        }
+    }
+}
