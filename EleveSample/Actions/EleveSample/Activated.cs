d121dbcda54886975d8c1fe9476055781f8646ec
basyura <basyura@gmail.com>
2019-05-05 17:46:55 +0900

    added Activated Sample

---
+++ EleveSample/Actions/EleveSample/Activated.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Eleve;
+
+namespace EleveSample.Actions.EleveSample
+{
+    public class Activated : EleveSampleActionBase
+    {
+        public override Task<ActionResult> Execute(object sender, EventArgs args, object obj)
+        {
+            ViewModel.Message = "This window has become 'Active'";
+
+            return SuccessTask;
+        }
+    }
+}
